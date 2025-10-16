ce2efa88d89652df2260e1c62495ee352e693db9
Xiaokuang Bai <baixiaokuang@gmail.com>
2025-10-16 10:10:55 +0800

    toUIMessageStream

--- main.ts
+++ main.ts
@@ -6,6 +6,6 @@ const model = google("gemini-2.5-flash");
 const prompt =
   "Give me the first paragraph of a story about an imaginary planet.";
 const result = streamText({ model, prompt });
-for await (const chunk of result.textStream) {
+for await (const chunk of result.toUIMessageStream()) {
   console.log(chunk);
 }
